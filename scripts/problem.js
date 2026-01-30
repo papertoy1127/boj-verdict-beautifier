55a3159fe506ce358dde8139e927846cdb2efc16
papertoy1127 <richard1909@naver.com>
2026-01-30 20:05:44 +0900

    Initial commit

---
+++ scripts/problem.js
@@ -0,0 +1,138 @@
+
+let isFirefox = false;
+try {
+    console.log(browser);
+    isFirefox = true;
+} catch { }
+
+console.log("injected!");
+console.log(isFirefox)
+
+
+const callback = (mutationsList, observer) => {
+    /** @type {Array<Element>} */
+    const allElems = Array.from(document.getElementsByClassName('result-text'));
+    allElems.forEach((verdictElem) => {
+        const cls = verdictElem.classList;
+        let link = '';
+        if (Element.prototype.isPrototypeOf(verdictElem.firstChild)) link = verdictElem.firstChild.getAttribute('href')
+        
+        if (cls.contains('result-ac')) {
+            verdictElem.className = "b-result b-result-ac";            
+            if (verdictElem.textContent == "맞았습니다!!") {
+                verdictElem.textContent = "AC";
+            } else if (verdictElem.textContent.startsWith("맞았습니다!!")) {
+                let ctn = verdictElem.textContent;
+                ctn = ctn.substring(9, ctn.length-1);
+                let pos = ctn.indexOf('/');
+                const cor = parseInt(ctn.substring(0, pos));
+                const tot = parseInt(ctn.substring(pos+1, ctn.length));
+
+                verdictElem.textContent = `${cor}/${tot}`
+            } else if (verdictElem.textContent.endsWith("점")) {
+                const ctn = verdictElem.textContent;
+                const cor = parseInt(ctn.substring(ctn, ctn.length-1));
+
+                verdictElem.textContent = `${cor}`
+            }
+        } else if (cls.contains('result-pac')) {
+            verdictElem.className = "b-result b-result-pac";
+            if (verdictElem.textContent == "맞았습니다!!") {
+                verdictElem.textContent = "PAC";
+            } else if (verdictElem.textContent.startsWith("맞았습니다!!")) {
+                let ctn = verdictElem.textContent;
+                ctn = ctn.substring(9, ctn.length-1);
+                let pos = ctn.indexOf('/');
+                const cor = parseInt(ctn.substring(0, pos));
+                const tot = parseInt(ctn.substring(pos+1, ctn.length));
+
+                verdictElem.textContent = `${cor}/${tot}`
+            } else if (verdictElem.textContent.endsWith("점")) {
+                const ctn = verdictElem.textContent;
+                const cor = parseInt(ctn.substring(ctn, ctn.length-1));
+
+                verdictElem.textContent = `${cor}`
+            }
+        } else if (cls.contains('result-rte')) {
+            verdictElem.className = "b-result b-result-rte";
+            if (verdictElem.textContent == "런타임 에러") {
+                verdictElem.textContent = "RTE";
+            } else if (verdictElem.textContent.startsWith("런타임 에러")) {
+                let ctn = verdictElem.textContent;
+                verdictElem.textContent = ctn.substring(8, ctn.length-1);
+            }
+        } else if (cls.contains('result-ce')) {
+            verdictElem.className = "b-result b-result-ce";
+            if (verdictElem.textContent == "컴파일 에러") {
+                verdictElem.textContent = "CE";
+            } else if (verdictElem.textContent.startsWith("컴파일 에러")) {
+                let ctn = verdictElem.textContent;
+                verdictElem.textContent = ctn.substring(8, ctn.length-1);
+            }
+        } else if (cls.contains('result-tle')) {
+            verdictElem.className = "b-result b-result-tle";
+            if (verdictElem.textContent == "시간 초과") {
+                verdictElem.textContent = "TLE";
+            }
+        } else if (cls.contains('result-mle')) {
+            verdictElem.className = "b-result b-result-mle";
+            if (verdictElem.textContent == "메모리 초과") {
+                verdictElem.textContent = "MLE";
+            }
+        } else if (cls.contains('result-ole')) {
+            verdictElem.className = "b-result b-result-ole";
+            if (verdictElem.textContent == "출력 초과") {
+                verdictElem.textContent = "OLE";
+            }
+        } else if (cls.contains('result-pe')) {
+            verdictElem.className = "b-result b-result-pe";
+            if (verdictElem.textContent == "출력 형식이 잘못되었습니다") {
+                verdictElem.textContent = "PE";
+            }
+        } else if (cls.contains('result-wa')) {
+            verdictElem.className = "b-result b-result-wa";
+            if (verdictElem.textContent == "틀렸습니다") {
+                verdictElem.textContent = "WA";
+            }
+        } else if (cls.contains('result-wait') && verdictElem.textContent != "Waiting...") {
+            verdictElem.className = "result-text result-wait b-result b-result-wj";
+            verdictElem.textContent = "Waiting...";
+        } else if (cls.contains('result-compile') && verdictElem.textContent != "Compiling...") {
+            verdictElem.className = "result-text result-compile b-result b-result-cmp";
+            verdictElem.textContent = "Compiling...";
+        } else if (cls.contains('result-judging') && !verdictElem.textContent.endsWith("%")) {
+            verdictElem.className = "result-text result-judging b-result b-result-ijd";
+
+            let ctn = verdictElem.textContent;
+            verdictElem.textContent = "-%";
+            if (ctn.startsWith("채점 중")) {
+                ctn = ctn.substring(6, ctn.length-1);
+                verdictElem.textContent = `${ctn}`
+            }
+        } else {
+            return;
+        }
+
+        
+        if (isFirefox) verdictElem.classList.add("b-corner-legacy");
+        else verdictElem.classList.add("b-corner-squircle")
+
+        if (link) {
+            const newElement = document.createElement('a');
+            newElement.className = verdictElem.className;
+            newElement.textContent = verdictElem.textContent;
+            newElement.href = link;
+            verdictElem.replaceWith(newElement);
+        }
+    });
+};
+
+const observer = new MutationObserver(callback);
+
+observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true
+});
+
+// document.addEventListener("DOMContentLoaded", (async () => {
+// }));
